e0135f083a2781920ff017348431d7e0c2dd9dab
Paulo Diogo <pndiogo@gmail.com>
2018-06-04 17:03:38 +0100

    JS implementation, CSS and HTML minor fixes and adaptations, images added

--- js/main.js
+++ js/main.js
@@ -4,66 +4,91 @@ const $cardCatarina = $('#catarina');
 const $cardInes = $('#ines');
 const $cardSara = $('#sara');
 
-// anniversaries in milliseconds
+// user anniversaries in milliseconds
 const persons = {
     paulo: {
         id: 'paulo',
         fullName: 'Paulo Diogo',
         date: '15 Fevereiro',
-        bDay: new Date('Feb 15, 2019 00:00:00').getTime(),
+        bDay: new Date('Feb 15, 2019 16:30:00').getTime(),
     },
     catarina: {
-        name: 'Catarina Lopes',
+        id: 'catarina',
+        fullName: 'Catarina Lopes',
+        date: '25 Fevereiro',
         bDay: new Date('Feb 25, 2019 00:00:00').getTime()
     },
     ines: {
-        name: 'Inês Diogo',
-        bDay: new Date('Jul 5, 2018 00:00:00').getTime()
+        id: 'ines',
+        fullName: 'Inês Diogo',
+        date: '5 Julho',
+        bDay: new Date('Jul 5, 2018 18:18:00').getTime()
     },
     sara: {
-        name: 'Sara Diogo',
-        bDay: new Date('Set 30, 2018 00:00:00').getTime()
+        id: 'sara',
+        fullName: 'Sara Diogo',
+        date: '30 Setembro',
+        bDay: new Date('Sep 30, 2018 05:55:00').getTime()
     },
-    insertNameAndDate: (idName) => {
+    insertNameAndDate: (user) => {
         // find card
-        const $card = $('#' + idName);
+        const $card = $('#' + user.id);
         // display full name
-        $card.find('.card-title').text(this.idName.fullName);
+        $card.find('.card-title').text(user.fullName);
         // display date
-        $card.find('.card-subTitle').text(this.idName.date);
+        $card.find('.card-subTitle').text(user.date);
     }
 }
 
-persons.insertNameAndDate(persons.paulo.id);
+// function invocations to add names and birthday date
+persons.insertNameAndDate(persons.paulo);
+persons.insertNameAndDate(persons.catarina);
+persons.insertNameAndDate(persons.ines);
+persons.insertNameAndDate(persons.sara);
 
 // update every second
 const intvl = setInterval(() => {
-    // get todays date and time (ms)
-    const now = new Date().getTime();
-    
-    // distance from now to launch date
-    const distance = persons.paulo.bDay - now;
-    
+
     // time calculations
-    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const mins = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((distance % (1000 * 60)) / (1000));
-    
-    // display result
-    $cardPaulo.find('.card-text').html(`
+    const dateCalculations = (user) => {
+        
+        // get todays date and time in milliseconds
+        const now = new Date().getTime();
+        // distance from now to birthday date
+        const date = user.bDay;
+        const distance = date - now;
+
+        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const mins = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((distance % (1000 * 60)) / (1000));
+
+        // display result
+        $('#' + user.id).find('.card-text').html(`
         <div>${days}<span> Days</span></div>
         <div>${hours}<span> Hours</span></div>
         <div>${mins}<span> Minutes</span></div>
         <div>${seconds}<span> Seconds</span></div>
-    `);
+        `);
 
-    // if launch date passed
-    if(distance < 0) {
-        // stop countdown
-        clearInterval(intvl);
-        // style and output text
-        countdown.style.color = '#17a2b8';
-        countdown.innerHTML = 'Parabéns!';
+        // update progressbar
+        const daysLeft = (((365 - days) * 100)/365).toFixed();
+        $('#' + user.id).find('.progress-bar').attr({
+            style: "width: " + daysLeft + "%",
+            "aria-valuenow": daysLeft
+        });
+
+        // if launch date passed
+        if(distance < 0) {
+            // stop countdown
+            clearInterval(intvl);
+            // style and output text
+            countdown.style.color = '#17a2b8';
+            countdown.innerHTML = 'Parabéns!';
+        }
     }
+    dateCalculations(persons.paulo);
+    dateCalculations(persons.catarina);
+    dateCalculations(persons.ines);
+    dateCalculations(persons.sara);
 }, 1000);
